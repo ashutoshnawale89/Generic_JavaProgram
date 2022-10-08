73bc3e62ed084ef71c44c38fd405fdca9eb27965
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-10-08 10:18:19 +0530

    Refactor: One Generic method For All Data Types

--- src/com/maximum/program/MaxProb.java
+++ src/com/maximum/program/MaxProb.java
@@ -1,43 +1,27 @@
 package com.maximum.program;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
-public class MaxProb {
-	public void MaxInteg() {
-		// Create the Array For comparing
-		Scanner sc =new Scanner(System.in);
-		String[] arr=new String[3];
-		//  Fill The Array
-		for (int i=0; i < arr.length;i++) {
-			System.out.println("Enter The String");
-			arr[i]=sc.next();
-		}
-		// compare the String Array
-		int num = arr[0].compareTo(arr[1]);
-		if (num < 0) {
-			num = arr[1].compareTo(arr[2]);
-			if (num <= 0) {
-				System.out.println("The Largest Word is  "+ arr[2] ); 
-			}
-			else  {
-				System.out.println("The Largest Word is  "+ arr[1] );	
-			}
-		}  
-		else {
-			num=(arr[0].compareTo(arr[2]));
-			if (num <= 0) {				
-				System.out.println("The Largest Word is  "+ arr[2] ); 
-			}
-			else {
-				System.out.println("The Largest Word is  "+ arr[0] );
-			}
-		}
-		sc.close();
-	}
-	public static void main(String[] args) {
 
-		MaxProb obj=new MaxProb();
-		obj.MaxInteg();
-	}	 
-}
+
+public class MaxProb {
+		public <E> void MaxInteg(E[] arr) {
+			Arrays.sort(arr); 
+			System.out.println(arr[arr.length-1]);   
+		}
+		public static void main(String[] args) {	
+			MaxProb obj=new MaxProb();
+			Integer [] intarr= {12,14,15};
+			Double [] doublearr= {12.5,14.5,15.5};
+			String[] str= {"ghg","njj","nnnknk"};
+			obj.MaxInteg(doublearr);
+			obj.MaxInteg(intarr);
+			obj.MaxInteg(str);
+			
+			
+		}
+			     
+				
+			}	 
 
