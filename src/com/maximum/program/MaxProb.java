426743c736746c76aa36496ffbd9266f97c0a1bb
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-10-08 09:46:00 +0530

    Added: Find Maximum String value

--- src/com/maximum/program/MaxProb.java
+++ src/com/maximum/program/MaxProb.java
@@ -6,22 +6,36 @@ public class MaxProb {
 	public void MaxInteg() {
 		// Create the Array For comparing
 		Scanner sc =new Scanner(System.in);
-		double[] arr=new double[3];
-		for (int i=0; arr.length > i; i++) {
-			System.out.println("Enter the array value ");
-			arr[i] = sc.nextDouble();
+		String[] arr=new String[3];
+		//  Fill The Array
+		for (int i=0; i < arr.length;i++) {
+			System.out.println("Enter The String");
+			arr[i]=sc.next();
 		}
-//    Find The Maximum Program
-		double max=arr[0];
-		for (int i=0; i < arr.length; i++) {
-			if (arr[i]> max) {
-				max=arr[i];
-			}	       
+		// compare the String Array
+		int num = arr[0].compareTo(arr[1]);
+		if (num < 0) {
+			num = arr[1].compareTo(arr[2]);
+			if (num <= 0) {
+				System.out.println("The Largest Word is  "+ arr[2] ); 
+			}
+			else  {
+				System.out.println("The Largest Word is  "+ arr[1] );	
+			}
+		}  
+		else {
+			num=(arr[0].compareTo(arr[2]));
+			if (num <= 0) {				
+				System.out.println("The Largest Word is  "+ arr[2] ); 
+			}
+			else {
+				System.out.println("The Largest Word is  "+ arr[0] );
+			}
 		}
-		System.out.println("The Largest Number is  "+max); 
 		sc.close();
 	}
-	public static void main(String[] args) {	
+	public static void main(String[] args) {
+
 		MaxProb obj=new MaxProb();
 		obj.MaxInteg();
 	}	 
