ddfd460da54c5a3a3eae8c6242f41d9a13ab371a
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-10-08 08:47:24 +0530

    Added: Find Maximum Integers value

--- src/com/maximum/program/MaxProb.java
+++ src/com/maximum/program/MaxProb.java
@@ -13,7 +13,7 @@ public class MaxProb {
 			System.out.println("Enter the array value ");
 			arr[i] = sc.nextInt();
 		}
-
+//    Find The Maximum Program
 		int max=arr[0];
 		for (int i=0; i < arr.length; i++) {
 			if (arr[i]> max) {
